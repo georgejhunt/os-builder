4f71005c6f677c8c52cdf17ffb04eefc4dbe57d2
george Hunt <georgejhunt@gmail.com>
2016-01-17 18:30:21 -0800

    add ZD conversion for images that already have the 2 partitions (versionedfs may not matter -- needs testing

--- modules/raw_image/mountfs.50.inject_image.sh
+++ modules/raw_image/mountfs.50.inject_image.sh
@@ -2,6 +2,8 @@
 # Licensed under the terms of the GNU GPL v2 or later; see COPYING for details.
 
 . $OOB__shlib
+
+target_img=$intermediatesdir/rawfs.img
 path=$(read_config raw_image path)
 echo "fsmount is $fsmount"
 if [ -z $fsmount -o -z $cachedir ]; then
@@ -12,6 +14,9 @@ echo "fsmount is $fsmount"
 
 umount $fsmount &>/dev/null || :
 rm -rf $fsmount/*
+
+echo "copying image to intermediate dir $target_img"
+dd if=$path of=$target_img bs=4M
 	
 echo "mounting image at $path to $fsmount"
 mkdir -p $fsmount
